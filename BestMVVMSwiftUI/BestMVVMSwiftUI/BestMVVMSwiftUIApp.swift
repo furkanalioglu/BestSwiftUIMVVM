8902a1f4b4e7c484591ed8ae77d17ce0c56a2aaf
Furkan Alioglu <91939823+furkanalioglu@users.noreply.github.com>
2023-10-20 20:05:39 +0300

    inital xcode setup

---
+++ BestMVVMSwiftUI/BestMVVMSwiftUI/BestMVVMSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestMVVMSwiftUIApp.swift
+//  BestMVVMSwiftUI
+//
+//  Created by Furkan Alioglu on 20.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct BestMVVMSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
