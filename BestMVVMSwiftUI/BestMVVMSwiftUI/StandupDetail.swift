65be976941f46d28a25a687737d2a0f30ad5240c
Furkan Alioglu <91939823+furkanalioglu@users.noreply.github.com>
2023-10-21 17:40:53 +0300

    removed deprecated action

--- BestMVVMSwiftUI/BestMVVMSwiftUI/StandupDetail.swift
+++ BestMVVMSwiftUI/BestMVVMSwiftUI/StandupDetail.swift
@@ -7,90 +7,176 @@
 
 import Foundation
 import SwiftUI
+import SwiftUINavigation
 
 final
 class StandupDetailModel : ObservableObject {
-    @Published var standup: Standup
-
-    init(standup: Standup) {
-      self.standup = standup
+    enum Destination {
+        case alert(AlertState<AlertAction>)
+        case meeting(Meeting)
     }
     
+    enum AlertAction {
+        case confirmDeletion
+    }
+    
+    @Published var destination: Destination?
+    @Published var standup: Standup
+    
+    init(destination: Destination? = nil ,
+         standup: Standup) {
+        self.destination = destination
+        self.standup = standup
+    }
+    
+    func deleteMeeting(atOffsets indices: IndexSet) {
+        self.standup.meetings.remove(atOffsets: indices)
+    }
+    
+    func meetingTapped(_ meeting: Meeting) {
+        self.destination = .meeting(meeting)
+    }
+    
+    func deleteButtonTapped() {
+        self.destination = .alert(.delete)
+    }
+    
+    func alertButtonTapped(_ action: AlertAction ){
+        print("asd")
+    }
 }
 
 struct StandupDetail : View {
     @ObservedObject var model: StandupDetailModel
-
+    
     var body: some View {
-      List {
-        Section {
-          Button {
-          } label: {
-            Label("Start Meeting", systemImage: "timer")
-              .font(.headline)
-              .foregroundColor(.accentColor)
-          }
-          HStack {
-            Label("Length", systemImage: "clock")
-            Spacer()
-            Text(self.model.standup.duration.formatted(
-              .units())
-            )
-          }
-
-          HStack {
-            Label("Theme", systemImage: "paintpalette")
-            Spacer()
-            Text(self.model.standup.theme.name)
-              .padding(4)
-              .foregroundColor(
-                self.model.standup.theme.accentColor
-              )
-              .background(self.model.standup.theme.mainColor)
-              .cornerRadius(4)
-          }
-        } header: {
-          Text("Standup Info")
-        }
-
-        if !self.model.standup.meetings.isEmpty {
-          Section {
-            ForEach(self.model.standup.meetings) { meeting in
-              Button {
-              } label: {
-                HStack {
-                  Image(systemName: "calendar")
-                  Text(meeting.date, style: .date)
-                  Text(meeting.date, style: .time)
+        List {
+            Section {
+                Button {
+                } label: {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
                 }
-              }
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text(self.model.standup.duration.formatted(
+                        .units())
+                    )
+                }
+                
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(self.model.standup.theme.name)
+                        .padding(4)
+                        .foregroundColor(
+                            self.model.standup.theme.accentColor
+                        )
+                        .background(self.model.standup.theme.mainColor)
+                        .cornerRadius(4)
+                }
+            } header: {
+                Text("Standup Info")
             }
-            .onDelete { indices in
+            
+            if !self.model.standup.meetings.isEmpty {
+                Section {
+                    ForEach(self.model.standup.meetings) { meeting in
+                        Button {
+                            self.model.meetingTapped(meeting)
+                        } label: {
+                            HStack {
+                                Image(systemName: "calendar")
+                                Text(meeting.date, style: .date)
+                                Text(meeting.date, style: .time)
+                            }
+                        }
+                    }
+                    .onDelete { indices in
+                        model.deleteMeeting(atOffsets: indices)
+                    }
+                } header: {
+                    Text("Past meetings")
+                }
             }
-          } header: {
-            Text("Past meetings")
-          }
+            
+            Section {
+                ForEach(self.model.standup.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            } header: {
+                Text("Attendees")
+            }
+            
+            Section {
+                Button("Delete") {
+                    self.model.deleteButtonTapped()
+                }
+                .foregroundColor(.red)
+                .frame(maxWidth: .infinity)
+            }
         }
+        .navigationTitle(self.model.standup.title)
+        .toolbar {
+            Button("Edit") {
+            }
+        }
+        .navigationDestination(unwrapping: self.$model.destination,
+                               case: CasePath(
+                                StandupDetailModel.Destination.meeting))
+        { $meeting in
+            MeetingView(meeting: meeting, standup: self.model.standup)
+        }
+        .alert(unwrapping: self.$model.destination,
+               case:  CasePath(
+                StandupDetailModel.Destination.alert),
+               action: { action in
+            if let actualAction = action {
+                self.model.alertButtonTapped(actualAction)
+            }
+        })
+        
 
-        Section {
-          ForEach(self.model.standup.attendees) { attendee in
-            Label(attendee.name, systemImage: "person")
-          }
-        } header: {
-          Text("Attendees")
-        }
+    }
+}
 
-        Section {
-          Button("Delete") {
-          }
-          .foregroundColor(.red)
-          .frame(maxWidth: .infinity)
+extension AlertState where Action == StandupDetailModel.AlertAction{
+    static let delete = AlertState(
+        title: TextState("Are you sure you want to delete"),
+        message: TextState("You can not recover your deleted meetings"),
+        buttons: [
+            .destructive(TextState("Yes"),
+                         action: .send(.confirmDeletion)),
+            .cancel(TextState("Nevermind"))
+        ]
+    )
+}
+
+struct MeetingView: View {
+    let meeting: Meeting
+    let standup: Standup
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading) {
+                Divider()
+                    .padding(.bottom)
+                Text("Attendees")
+                    .font(.headline)
+                ForEach(self.standup.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                Text("Transcript")
+                    .font(.headline)
+                    .padding(.top)
+                Text(self.meeting.transcript)
+            }
         }
-      }
-      .navigationTitle(self.model.standup.title)
-      .toolbar {
-        Button("Edit") {
-        }
-      }
+        .navigationTitle(
+            Text(self.meeting.date, style: .date)
+        )
+        .padding()
     }
 }
